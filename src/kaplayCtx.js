df6ce81cc3a07bf11713337ca4ff8e8384cd0e37
vijaysinghkadela <132544545+vijaysinghkadela@users.noreply.github.com>
2024-09-05 14:41:32 +0530

    final commit

---
+++ src/kaplayCtx.js
@@ -0,0 +1,19 @@
+import kaplay from "kaplay";
+
+const k = kaplay({
+  width: 1920,
+  height: 1080,
+  letterbox: true,
+  background: [0, 0, 0],
+  global: false,
+  touchToMouse: true,
+  buttons: {
+    jump: {
+      keyboard: ["space"],
+      mouse: "left",
+    },
+  },
+  debugKey: "f10",
+  debug: true,
+});
+export default k;
